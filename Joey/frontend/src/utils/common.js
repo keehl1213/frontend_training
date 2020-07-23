442189c0ba70feda25cdd1fb79df431fd7094133
Alston Yang <Alston_Yang@wistron.com>
2020-07-23 14:46:48 +0800

    delete folder

--- Joey/frontend/src/utils/common.js
+++
@@ -1,8 +0,0 @@
-export const createUniqueID = (array = []) => {
-  const idArr = array.map((item) => item.id);
-  if (idArr && idArr.length > 0) {
-    return Math.max(...idArr) + 1;
-  }
-
-  return 1;
-};

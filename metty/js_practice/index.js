d4f7390871f03a29be3bba793fd3ebc6a28bc146
WHQ\10909303 <metty_chang@wistron.com>
2020-09-21 17:54:34 +0800

    Object & Regexp 1.3.4.5 answer

--- metty/js_practice/index.js
+++ metty/js_practice/index.js
@@ -1,14 +1,3 @@
-// import data from './data.json';
-
-
-
-// let find1 = data.reduce(function(accumulator, currentValue, currentIndex, array){
-//    console.log(accumulator,currentValue)
-//     return Math.max(accumulator, currentValue.price);;
-// },0);
-
-// console.log(find1);
-
 import string from './string';
 import object from './object_Regexp';
 
@@ -19,11 +8,16 @@ var a = {
     text: 'aaa',
     book:{aaa:'1', bbb:'2'}
 };
+let e1='wistron@wistron.com';
+let e2='wistron.com';
 
-
-object.work1(a);  //deep clone 結果:aaa
 
 console.log(string.work1(s,t)); //判斷重組字 結果:false
 console.log(string.work2(str)); //字串反轉 結果:olleh
+console.log(object.work1(a));//deep clone 結果:{aaa:'1', bbb:'2'}
 
-
+var john = new object.Person('john', 18); //class constructor for Person
+console.log(john.sayhi()); //hi I'm john, 18 years old.
+console.log(object.replaceAll('abacadaeaf', 'a', '123')); //123b123c123d123e123f
+console.log(object.email_format(e1)); //true
+console.log(object.email_format(e2)); //false
--- metty/js_practice/object_Regexp.js
+++ metty/js_practice/object_Regexp.js
@@ -1,13 +1,56 @@
 const work1 = (a) => {
 
-    var b = Object.assign({}, a); //先建立一個空物件，再把a的物件複製過去
+    let b_str = JSON.stringify(a);//把物件轉字串
+    let b = JSON.parse(b_str);    //把字串轉物件
     b.text = 'bbb';
     b.book.aaa = '3';
-    console.log(a.book);
+    return a.book;
 
 }
 
+// lodash
+// const work1_1 = (a) => {
+//     let b = _.cloneDeep(objects);
+//     b.text = 'bbb';
+//     b.book.aaa = '3';
+//     return a.book;
+// } 
+
+
+class Person { //class constructor for Person
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayhi() {
+        return `hi I'm ${this.name}, ${this.age} years old.`;
+    }
+}
+
+function replaceAll(string, search, replace) { //regexp replace all
+
+    const result = string.replace(new RegExp(search, 'g'), replace);
+    return result;
+
+}
+
+const email_format = (e) => {
+    const emailRule = /^\w+((-\w+)|(\.\w+))*\@[\w]+((\.|-)[\w]+)*\.[A-Za-z]+$/; 
+    //開頭必須是英文或數字
+    //@前可以出現 1 個以上的文字、數字或「-」的組合  或者  出現 1 個以上的文字、數字或「.」的組合
+    //中間一定有@
+    //@後只能出現「.」或是「-」，但這兩個字元不能連續時出現
+    if (!emailRule.test(e)) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 export default {
     work1,
+    Person,
+    replaceAll,
+    email_format
 }

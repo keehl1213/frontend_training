bab284f342f695a784875422a2a838fde6b3b829
WHQ\10909303 <metty_chang@wistron.com>
2020-09-21 13:31:19 +0800

    add string work

---
+++ metty/js_practice/index.js
@@ -0,0 +1,17 @@
+// import data from './data.json';
+
+
+
+// let find1 = data.reduce(function(accumulator, currentValue, currentIndex, array){
+//    console.log(accumulator,currentValue)
+//     return Math.max(accumulator, currentValue.price);;
+// },0);
+
+// console.log(find1);
+
+import string from './string';
+
+string.test();
+string.test222();
+
+
---
+++ metty/js_practice/string.js
@@ -0,0 +1,12 @@
+const test = () => {
+    console.log('TTTTTT');
+}
+
+const test222 = () => {
+    console.log('TTTTTT2222');
+}
+
+export default {
+    test,
+    test222,
+}
